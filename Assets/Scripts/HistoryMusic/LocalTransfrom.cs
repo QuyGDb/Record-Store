99fc9158cc2ff2965d1fdb62c670c038cfa49ace
Mitsuha <trinhxuanquy2k3@gmail.com>
2025-04-03 21:53:35 +0700

    cc

---
+++ Assets/Scripts/HistoryMusic/LocalTransfrom.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class LocalTransfrom
+{
+    public Vector3 localPosition;
+    public Quaternion localRotation;
+    public Vector3 localScale;
+}
